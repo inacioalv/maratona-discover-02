1fcc6011e49acd12e2dc4c6b778f7583365657f9
inacioalv <51245154+inacioalv@users.noreply.github.com>
2021-04-05 14:34:54 -0300

    aula-02

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ server.set("view engine","ejs")
 
 server.use(express.static("public"))
 
+//usar request.body
+server.use(express.urlencoded({extended:true}))
+
 server.use(routes)
+
 
 server.listen(3300, ()=> console.log("Serve"))
